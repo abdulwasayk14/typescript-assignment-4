d2ac1b6c915a8eef2e718c7fc57d8ed3397e741c
Abdul wasay khan <156195187+abdulwasayk14@users.noreply.github.com>
2024-02-27 00:59:15 +0500

    Add files via upload

---
+++ Famous_Quote.js
@@ -0,0 +1,4 @@
+"use strict";
+let famous_person = 'Quaid e Azam';
+let message = '“You are free to go to your mosque,temples and whereever you want ”';
+console.log(`${famous_person} once said, ${message} `);
---
+++ Famous_Quote.ts
@@ -0,0 +1,5 @@
+let  famous_person :string = 'Quaid e Azam';
+
+let message :string = '“You are free to go to your mosque,temples and whereever you want ”';
+
+console.log(`${famous_person} once said, ${message} `);
